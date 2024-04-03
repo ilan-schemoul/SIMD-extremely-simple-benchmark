1eee7e046b0435b8da2027fe04a5057ec6e64d43
ilan schemoul <ilanschemoul@gmail.com>
2024-04-03 18:39:13 +0200

    Add new method to also test structure of arrays without SIMD

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# What is this ?
+# What is this
 This code compares the performances of AVX-2 versus scalar (regular `for` loops we all know) for calculating the sum of larges values (3 millions elements computed 100 times).
 
-I have never used SIMD. I was intrigued by this piece of hardware that can increase your performances more than tenfold and most of us software engineers don't know !
+I did not know how to use SIMD but I knew it can really help performances and I also wanted to compare how efficiency of data structures (structure of array vs array of structure) can impact performances. So this is my experimentation to compare data structures and the use of SIMD.
 
 # How to run ?
 `g++ -O2 -march=native a.cpp -o avx && ./avx`
@@ -9,15 +9,41 @@ I have never used SIMD. I was intrigued by this piece of hardware that can incre
 You need AVX 2 support. You can customize the values of the macros at the top of the file (`SIZE_256` and `NUMBER_OF_SUM_RUN`)
 
 # Results ?
-On my Ryzen 6800U (decent CPU for laptop) computing 100 times the sum of 3 millions 256 bits elements took :
-- -O0: 5080ms without SIMD, 1687ms with SIMD
-- -O2: 2437ms without SIMD, 507ms with SIMD
-- -O3: 575ms without SIMD, 858ms with SIMD
+I run these tests on my Ryzen 6800U (decent CPU for laptop). I computed the sum of 24 millions 32 bits integer a 100 times in a row.
+
+## -02
+Array of structures \
+I initialization=99ms sum=2482ms
+
+Structure of array without SIMD \
+I initialization=78ms sum=850ms
+
+Structure of array with SIMD \
+I initialization=994ms sum=474ms
+
+## -03
+Array of structures \
+I initialization=93ms sum=578ms
+
+Structure of array without SIMD \
+I initialization=94ms sum=270ms
+
+Structure of array with SIMD \
+I initialization=469ms sum=599ms
 
 # Conclusions ?
-- If I read that something is really hard I will do it (even when it means using gentoo for a year :( ).
-- Hard to beat -O3 if you don't know what you're doing.
-- But still so many people don't use -O3 because it lacks reliability so a nearly 5x increase in performances is still outstanding for -O2 ! And you can get much more on AVX-512, if you use smaller than 32 bits data and if you know what you're doing (unlike me)
-- SIMD is not impossible to do but there's barely any good documentation (same as writing in assembly, or writing a Kernel) and it works quite differently than regular code. Not very hard but not intuitive.
-- Compilers can't always unroll loops and most devs don't know how to use SIMD. So we sit with massively unused performances ?
-- I don't think we have abstractions over SIMD instructions. So we have to learn every instructions of every family of CPU for max performances ?
+
+## SIMD
+- I initialization is slower (can be because I don't use aligned addresses) but sum is faster with SIMD on -O2.
+- -O3 is able to unroll the loop and its as fast as the SIMD version and much faster in initialization when you use the right data structure.
+- Compiler cannot always unroll the loop and -O2 is more reliable than -O3 so writing SIMD instructions can still make sense.
+- I don't think we have abstractions over SIMD instructions. So we have to learn every instructions of every family of CPU for max performances ? Seems like a massive cost and technical debt to me but if you writing say a game engine I can understand why you would still do it. What I mean is you need a strong case to justify the technical debt brought by SIMD.
+- SIMD is not impossible to do but there's barely any comprehensive and friendly documentation (same as writing in assembly, or writing a Kernel) and it works quite differently than regular code. As I already said probably focus on data structures and check if your compiler can help you with SIMD.
+
+## Efficiency of the structure of array
+ Compiler can unroll the loop (sometimes) but can't transform your data structure (AFAIK). Writing a structure of array instead of an array of structure (check the code to understand what it means) can bring massive performances (3x for O2 in my case).
+
+If you have a structure containing x y z an array of this structure will look like this in your RAM |xyzxyzxyzxyzxyz|. \
+When CPU accesses RAM it doesn't just load that one element you asked for but also a few KB/MB of the elements after that. But if you only access x in |xyzxyzxyzxyz| you are wasting 2/3 of your cache (so your CPU has to make 3 times more call to the RAM !) \
+That's why you need a struct of array (`struct { int x[250]; int y[250]; int z[250] })`) so all "x" "y" and "z" are stored contiguously. In RAM it looks like this |xxxxxxxxxx| and when the CPU loads a part of the RAM in its cache it can fully utilize it (in this case we need to make 3 times less calls to RAM !). \
+Of course this only applies when you only access one field of a struct but I do not think it is an infrequent case.
--- main.cpp
+++ main.cpp
@@ -17,13 +17,19 @@ struct RegularStruct {
   uint32_t z;
 };
 
-struct StructOfArrays {
+struct StructOfArrays32Bits {
+  std::vector<uint32_t> x_vec;
+  std::vector<uint32_t> y_vec;
+  std::vector<uint32_t> z_vec;
+};
+
+struct StructOfArrays256Bits {
   std::vector<__m256i> x_vec;
   std::vector<__m256i> y_vec;
   std::vector<__m256i> z_vec;
 };
 
-uint64_t sum(const std::vector<struct RegularStruct> &threeDimensions) {
+uint64_t sum_aos(const std::vector<struct RegularStruct> &threeDimensions) {
   uint64_t sum = 0;
   for (uint64_t i = 0; i < SIZE_32; i++) {
     sum += threeDimensions[i].x;
@@ -31,7 +37,15 @@ uint64_t sum(const std::vector<struct RegularStruct> &threeDimensions) {
   return sum;
 }
 
-uint64_t sum_2(const StructOfArrays &structOfArrays) {
+uint64_t sum_soa_32(const StructOfArrays32Bits &structOfArrays32Bits) {
+  uint64_t sum = 0;
+  for (uint64_t i = 0; i < SIZE_32; i++) {
+    sum += structOfArrays32Bits.x_vec[i];
+  }
+  return sum;
+}
+
+uint64_t sum_simd(const StructOfArrays256Bits &structOfArrays256Bits) {
   uint64_t zeroes[4] = {0, 0, 0, 0};
 
   // In loadu the u stands for unaligned because pointers by defaults
@@ -39,7 +53,7 @@ uint64_t sum_2(const StructOfArrays &structOfArrays) {
   __m256i sum = _mm256_loadu_si256((__m256i *)zeroes);
 
   for (uint64_t i = 0; i < SIZE_256; i++) {
-    sum = _mm256_add_epi32(sum, structOfArrays.x_vec[i]);
+    sum = _mm256_add_epi32(sum, structOfArrays256Bits.x_vec[i]);
   }
 
   uint32_t final_sum[8] = {0, 0, 0, 0, 0, 0, 0, 0};
@@ -67,11 +81,27 @@ const std::vector<struct RegularStruct> initArrayOfStruct() {
   return regularStruct;
 }
 
-const struct StructOfArrays initStructOfArrays() {
-  StructOfArrays structOfArrays;
-  structOfArrays.x_vec.reserve(SIZE_256);
-  structOfArrays.y_vec.reserve(SIZE_256);
-  structOfArrays.z_vec.reserve(SIZE_256);
+const struct StructOfArrays32Bits initStructOfArrays32Bits() {
+  StructOfArrays32Bits structOfArrays32Bits;
+
+  structOfArrays32Bits.x_vec.reserve(SIZE_32);
+  structOfArrays32Bits.y_vec.reserve(SIZE_32);
+  structOfArrays32Bits.z_vec.reserve(SIZE_32);
+
+  for (uint64_t i = 0; i < SIZE_32; i++) {
+    structOfArrays32Bits.x_vec[i] = (2);
+    structOfArrays32Bits.y_vec[i] = (2);
+    structOfArrays32Bits.z_vec[i] = (3);
+  }
+
+  return structOfArrays32Bits;
+}
+
+const struct StructOfArrays256Bits initStructOfArrays256Bits() {
+  StructOfArrays256Bits structOfArrays256Bits;
+  structOfArrays256Bits.x_vec.reserve(SIZE_256);
+  structOfArrays256Bits.y_vec.reserve(SIZE_256);
+  structOfArrays256Bits.z_vec.reserve(SIZE_256);
 
   uint32_t x_array[256 / 32];
   for (uint64_t i = 0; i < 256 / 32; i++)
@@ -86,12 +116,12 @@ const struct StructOfArrays initStructOfArrays() {
     z_array[i] = 2;
 
   for (uint64_t i = 0; i < SIZE_256; i++) {
-    structOfArrays.x_vec[i] = _mm256_loadu_si256((__m256i const *)x_array);
-    structOfArrays.y_vec[i] = _mm256_loadu_si256((__m256i const *)y_array);
-    structOfArrays.z_vec[i] = _mm256_loadu_si256((__m256i const *)z_array);
+    structOfArrays256Bits.x_vec[i] = _mm256_loadu_si256((__m256i const *)x_array);
+    structOfArrays256Bits.y_vec[i] = _mm256_loadu_si256((__m256i const *)y_array);
+    structOfArrays256Bits.z_vec[i] = _mm256_loadu_si256((__m256i const *)z_array);
   }
 
-  return structOfArrays;
+  return structOfArrays256Bits;
 }
 
 int main(int argc, char *argv[]) {
@@ -99,40 +129,66 @@ int main(int argc, char *argv[]) {
 
   const auto begin = high_resolution_clock::now();
 
+  // Array of struct, no SIMD
   const std::vector<struct RegularStruct> arrayOfStruct = initArrayOfStruct();
 
-  const auto reserve_1 = high_resolution_clock::now();
+  const auto init_aos = high_resolution_clock::now();
 
-  uint64_t x1 = 0;
+  uint64_t x_aos = 0;
   for (uint64_t i = 0; i < NUMBER_OF_SUM_RUN; i++)
-    x1 += sum(arrayOfStruct);
+    x_aos += sum_aos(arrayOfStruct);
 
-  const auto sum_1 = high_resolution_clock::now();
+  const auto sum_aos = high_resolution_clock::now();
 
-  const StructOfArrays structOfArrays = initStructOfArrays();
+  // Struct of arrays, no SIMD
+  const StructOfArrays32Bits structOfArrays32Bits = initStructOfArrays32Bits();
 
-  const auto reserve_2 = high_resolution_clock::now();
+  const auto ini_soa_32 = high_resolution_clock::now();
 
-  uint64_t x2 = 0;
+  uint64_t x_soa_32 = 0;
+  for (uint64_t i = 0; i < NUMBER_OF_SUM_RUN; i++)
+    x_soa_32 += sum_soa_32(structOfArrays32Bits);
+
+  const auto sum_soa_32 = high_resolution_clock::now();
+
+  // Struct of arrays, no SIMD
+  const StructOfArrays256Bits structOfArrays256Bits = initStructOfArrays256Bits();
+
+  const auto init_soa_256 = high_resolution_clock::now();
+
+  uint64_t x_soa_256 = 0;
   for (int i = 0; i < NUMBER_OF_SUM_RUN; i++)
-    x2 += sum_2(structOfArrays);
+    x_soa_256 += sum_simd(structOfArrays256Bits);
 
-  const auto sum_2 = high_resolution_clock::now();
+  const auto sum_soa_256 = high_resolution_clock::now();
 
-  std::cout << "reserve_1="
-            << duration_cast<milliseconds>(reserve_1 - begin).count()
-            << "ms sum_1="
-            << duration_cast<milliseconds>(sum_1 - reserve_1).count() << "ms"
-            << std::endl;
+  std::cout << "Array of structures " << std::endl
+            << "Initialization="
+            << duration_cast<milliseconds>(init_aos - begin).count()
+            << "ms sum="
+            << duration_cast<milliseconds>(sum_aos - init_aos).count() << "ms"
+            << std::endl << std::endl;
 
-  std::cout << "reserve_2="
-            << duration_cast<milliseconds>(reserve_2 - sum_1).count()
-            << "ms sum_2="
-            << duration_cast<milliseconds>(sum_2 - reserve_2).count() << "ms"
-            << std::endl;
+  std::cout << "Structure of arrays without SIMD" << std::endl
+            << "Initialization="
+            << duration_cast<milliseconds>(ini_soa_32 - sum_aos).count()
+            << "ms sum="
+            << duration_cast<milliseconds>(sum_soa_32 - ini_soa_32).count() << "ms"
+            << std::endl << std::endl;
 
-  if (x1 != x2)
-    std::cout << " x1 != x2, x1=" << x1 << " x2=" << x2 << std::endl;
+  std::cout << "Structure of arrays with SIMD" << std::endl
+            << "Initialization="
+            << duration_cast<milliseconds>(init_soa_256 - sum_aos).count()
+            << "ms sum="
+            << duration_cast<milliseconds>(sum_soa_256 - init_soa_256).count() << "ms"
+            << std::endl << std::endl;
+
+  if (x_aos != x_soa_256 || x_soa_32 != x_soa_256) {
+    std::cout << "Test failed. Results are different x_aos=" << x_aos << " x_soa_32=" << x_soa_32 << " x_soa_256=" << x_soa_256 << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  std::cout << "Test succeeded (all values are the same). Result is " << x_aos << "." << std::endl;
 
   return 0;
 }
